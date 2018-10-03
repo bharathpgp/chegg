d812badfee408119db7b9f46b267d6698c030bfd
Jac08H <jac08h@protonmail.com>
2018-10-03 20:03:36 +0200

    Put functionality into standalone function

--- python/movie.py
+++ python/movie.py
@@ -1,14 +1,37 @@
-file_ref = open("movies.txt", 'r')
+def movies_by_year(filename, year, print_results=True):
+    """
+    Args:
+         filename: str
+         year: int
+         print_results: bool, if True the functions prints results
+    Returns:
+        list of movies from given year, None if invalid year is provided or file does not exist
+    """
+    MIN_YEAR = 1880
+    MAX_YEAR = 2050
+    DELIMETER = '\t'
+
+    if year < MIN_YEAR or MAX_YEAR < year :
+        print('Invalid year. Please enter year in range {min_year}-{max_year}'.format(min_year=MIN_YEAR,
+                                                                                      max_year=MAX_YEAR))
+        return
+
+    try:
+        with open(filename) as fp:
+            result_movies = []
+            lines = fp.readlines()
+            for movie in lines:
+                movie_data = movie.split(DELIMETER)
+                if int(movie_data[1] == year_input):
+                    result_movies.append(movie_data[0])
+        if print_results:
+            print('\n'.join(result_movies))
+
+        return result_movies
+
+    except FileNotFoundError:
+        print('File does not exist.')
+        return
+
 year_input = int(input("Please enter a year from 1880-2050:"))
-if year_input < 1880 or year_input > 2050:
-    print("Invalid year")
-else:
-    line = file_ref.readline()  # skip the first line(header) from movies.txt
-    line = file_ref.readline()
-    while line:
-        line = line.strip('\n')  # removing '\n' from each record
-        movie_data = line.split('\t')  # splittng each record with '\t' delimiter
-        if int(movie_data[1]) == year_input:
-            print(movie_data[0])
-        line = file_ref.readline()
-file_ref.close()
+movies_by_year('movies.txt', year_input)
