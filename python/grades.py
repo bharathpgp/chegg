59acbf8e7084cfcb606d5bf62345da16f92dfb0c
HUMBY <dfxz58@durham.ac.uk>
2018-10-03 22:51:57 +0100

    improved grades.py

--- python/grades.py
+++ python/grades.py
@@ -2,44 +2,23 @@
 import matplotlib.pyplot as plt
 
 # path of the students grade file
-
 file = '/tmp/grades.txt'
 
 # initial count of each grade
-
-ranks = {
-    'A': 0,
-    'B': 0,
-    'C': 0,
-    'D': 0,
-    'F': 0,
-    }
+ranks = { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'F': 0 }
+boundry = { 10: 'A', 9: 'A', 8: 'B', 7:'C', 6: 'D' }
 
 # readind grades from file and stored into list
-
 data = [line.strip() for line in open(file, 'r')]
 
 # calculating number of students in each grade
-
 for studGrade in data:
     grades = list(map(int, studGrade.split(',')))
-    totGrade = 0
-    for grade in grades:
-        totGrade += grade
-    avg = totGrade / 5.0
-    if avg >= 90 and avg <= 100:
-        ranks['A'] += 1
-    elif avg >= 80 and avg <= 89:
-        ranks['B'] += 1
-    elif avg >= 70 and avg <= 79:
-        ranks['C'] += 1
-    elif avg >= 60 and avg <= 69:
-        ranks['D'] += 1
-    else:
-        ranks['F'] += 1
+    avg = sum(grades) / len(grades)
+    mark = boundry.get(int(avg/10),'F')
+    ranks[mark] += 1
 
 # drawing bar chart
-
 (fig, ax) = plt.subplots()
 ax.set_title('Student Grades')
 ax.set_xlabel('Grade Category')
