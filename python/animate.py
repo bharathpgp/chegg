75dea425ab9699921bf903b3f9c33b3fea0c1d60
Swapnil Jha <swapniljha001@gmail.com>
2020-09-25 06:03:50 +0530

    Update animate.py
    Improved readability in docstrings.

--- python/animate.py
+++ python/animate.py
@@ -1,50 +1,61 @@
 from graphics import *
 from time import sleep
 
-
-def bounce(gold_point, dx, dy):  # Function 'bounces' ball off wall
-    center = gold_point.getCenter()  # get center of ball
-    if center.getX() not in range(10, 290):  # within window?
-        dx *= -1  # change direction
+def bounce(gold_point, dx, dy):
+    # Function 'bounces' ball off wall
+    
+    center = gold_point.getCenter()
+    # Get the center of the ball
+    
+    if center.getX() not in range(10, 290):
+        # Is the center within the window?
+        dx *= -1        # Changes the direction
+        
     if center.getY() not in range(10, 290):
         dy *= -1
-    return dx, dy  # return new values
-
+    return dx, dy
 
 def main():
     window = GraphWin("Win", 300, 300)
     radius = 15
-    gold_point = Circle(Point(150, 150), radius)  # make ball
+    gold_point = Circle(Point(150, 150), radius)  # Make ball
     gold_point.setFill('gold')
     gold_point.draw(window)
-    dx, dy = 3, -5
-
-    # initial direction
+    dx, dy = 3, -5      # Initial direction
     while not window.checkMouse():  # repeat until mouse click
-        gold_point.move(dx, dy)  # move ball
-        # there is no direct way to change the radius of the circle, so we need to
-        # undraw the current object, re initialize the object with reduced radius,
-        # and draw it again
-
-        # getting center of current ball
+        gold_point.move(dx, dy)
+        """ Move the ball.
+        There is no direct way to change the radius of the circle, so we need to remove
+        the current object, and re-initialize the object with reduced radius,
+        and draw it again."""
+        
         center = gold_point.getCenter()
-        # undrawing old ball
-        gold_point.undraw()
-        # creating new ball with above center coordinates and with reduced radius
-        gold_point = Circle(center, radius)
-        gold_point.setFill('gold')
-        # drawing it again
-        gold_point.draw(window)
-        # reducing the radius by 0.05 for the next iteration
-        radius -= 0.05
-        # preventing radius being zero or negative
-        if radius <= 0:
-            radius = 0.05
-        sleep(0.02)  # sleep 0.02 seconds
-        dx, dy = bounce(gold_point, dx, dy)
-        # call function to check
-    window.getMouse()
-    window.close()  # close windows
+        # Getting the center of current ball
 
+        gold_point.undraw()
+        # Removing the old ball
+        
+        gold_point = Circle(center, radius)
+        # Creating new ball with above center coordinates and with reduced radius
+        
+        gold_point.setFill('gold')
+        
+        gold_point.draw(window)
+        # Re-drawing the ball
+        
+        radius -= 0.05
+        # Reducing the radius by 0.05 for the next iteration
+        
+        if radius <= 0:
+            # Preventing radius being zero or negative
+            radius = 0.05
+        
+        sleep(0.02)     # Sleep 0.02 seconds
+        
+        dx, dy = bounce(gold_point, dx, dy)
+        # Call function to check
+        
+    window.getMouse()
+    window.close()  # Close the Window
 
 main()
